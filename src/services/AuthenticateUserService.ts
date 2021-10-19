93a9dba09eaba57a1e3c05003fab2a3c3cd7e91a
wsasouza <wsasouza@hotmail.com>
2021-10-19 00:15:51 -0300

    add - github access_token

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+/**
+ * Receber code(string)
+ * Recuperar o access_token no github
+ * Verificar se o usuário existe no DB
+ * > SIM = gerar um token
+ * > NÃO = salvar o usuario no DB e gerar um token
+ * Retornar o token com as infos do user
+ */
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "https://github.com/login/oauth/access_token";
+
+    const response = await axios.post(url, null, {
+      params: {
+        client_id: process.env.GITHUB_CLIENT_ID,
+        client_secret: process.env.GITHUB_CLIENT_SECRET,
+        code,
+      },
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+
+    return response.data;
+
+  }
+}
+
+export { AuthenticateUserService }
