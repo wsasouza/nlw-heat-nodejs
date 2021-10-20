e4cc061ac511755300752ebe8e8965eac2d04b98
wsasouza <wsasouza@hotmail.com>
2021-10-20 19:52:11 -0300

    API Node completed

---
+++ src/server.ts
@@ -0,0 +1,4 @@
+import { serverHttp } from './app';
+
+
+serverHttp.listen(4000, () => console.log('🚀 Server is running on PORT 4000'));
--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,5 +1,5 @@
 import prismaClient from '../prisma';
-
+import { io } from '../app';
 class CreateMessageService {
   async execute(text: string, user_id: string) {
     const message = await prismaClient.message.create({
@@ -10,7 +10,19 @@ class CreateMessageService {
       include: {
         user: true
       }
-    })
+    });
+
+    const infoWS = {
+      text: message.text,
+      user_id: message.user_id,
+      created_at: message.created_at,
+      user: {
+        name: message.user,
+        avatar_url: message.user.avatar_url
+      }
+    }
+
+    io.emit('new_message', infoWS);
 
     return message;
   }
