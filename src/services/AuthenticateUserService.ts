faad6000ddec665dcb5a6137c98589d91600e243
wsasouza <wsasouza@hotmail.com>
2021-10-19 23:22:03 -0300

    create migration messages - Prisma

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,8 @@
 import axios from 'axios';
+import prismaClient from '../prisma';
+import { sign } from 'jsonwebtoken';
+
+
 
 /**
  * Receber code(string)
@@ -42,8 +46,41 @@ class AuthenticateUserService {
       }
     });
 
+    const { login, id, avatar_url, name } = response.data;
 
-    return response.data;
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    })
+
+    if(!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name
+        }
+      })
+    }
+
+    const token = sign(
+      {
+        user: {
+          name: user.name,
+          avatar_url: user.avatar_url,
+          id: user.id,
+        },
+      }, 
+      process.env.JWT_SECRET,
+      {
+        subject: user.id,
+        expiresIn: '1d'
+      }
+    );
+
+    return { token, user };
   }
 }
 
